20b87ba4b4ae419cdd53baebf60f0f5df560f613
Mark Tereshchenko <astr6.6.6@yandex.ru>
2025-03-09 15:53:42 +0300

    Подсушил код

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,70 +1,26 @@
-from datetime import datetime, timedelta
-from jose import jwt
 from sanic import Blueprint, text
 from sanic.exceptions import SanicException
-from sanic.response import json as sanic_json
-from app.database.config import get_async_session
-from app.core.config import settings
-from app.daos.user_dao import UserDAO
-from app.schemas.user_shema import LoginRequest, LoginResponse, TokenPayload
+from app.schemas.user_shema import LoginRequest
 from pydantic import ValidationError
-
+from app.utils.auth_utils import get_jwt_response
 
 auth_bp = Blueprint("auth", url_prefix="/auth")
-JWT_SECRET, JWT_ALGORITHM, JWT_EXP = settings.JWT_CONFIG
 
 @auth_bp.post("/login")
 async def login(request):
-    data = request.json
+    payload = request.json
 
     try:
-        LoginRequest(**data)
+        LoginRequest(**payload)
     except ValidationError as e:
         raise SanicException(
             message=f"Invalid request data: {e.errors()}",
             status_code=400
         )
+    email = payload.get("email")
+    password = payload.get("password")
 
-    email = data.get("email")
-    password = data.get("password")
-
-    if not email or not password:
-        raise SanicException("Email or password is missing", status_code=400)
-
-    async with get_async_session() as session:
-        user_dao = UserDAO(session)
-        user = await user_dao.get_user_by_email(email)
-
-        if not user or not user.check_password(password):
-            raise SanicException("Incorrect email or password", status_code=401)
-        
-        if not user.is_active:
-            raise SanicException("User is not active", status_code=403)
-
-    token_payload = TokenPayload(
-        sub=str(user.id),
-        exp=datetime.utcnow() + timedelta(seconds=JWT_EXP)
-    )
-
-    token = jwt.encode(
-        token_payload.model_dump(),
-        JWT_SECRET,
-        algorithm=JWT_ALGORITHM
-    )
-
-    response = sanic_json(LoginResponse(
-        message="Login successful",
-        user_id=str(user.id)
-    ).model_dump())
-
-    response.add_cookie(
-        key="access_token",
-        value=token,
-        httponly=True,
-        max_age=JWT_EXP,
-        samesite="Strict",
-        path="/",
-    )
+    response = await get_jwt_response(email, password)
 
     return response
 
---
+++ app/utils/auth_utils.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from app.schemas.user_shema import LoginResponse, TokenPayload
+from app.core.config import settings
+from sanic.response import json as sanic_json
+from app.database.config import get_async_session
+from app.daos.user_dao import UserDAO
+from sanic.exceptions import SanicException
+
+
+async def validate_user_payload(email, password):
+    if not email or not password:
+        raise SanicException("Email or password is missing", status_code=400)
+
+    async with get_async_session() as session:
+        user_dao = UserDAO(session)
+        user = await user_dao.get_user_by_email(email)
+
+        if not user or not user.check_password(password):
+            raise SanicException("Incorrect email or password", status_code=401)
+        
+        if not user.is_active:
+            raise SanicException("User is not active", status_code=403)
+
+        return user
+
+async def get_jwt_response(email, password):
+    JWT_SECRET, JWT_ALGORITHM, JWT_EXP = settings.JWT_CONFIG
+    user = await validate_user_payload(email, password)
+
+    token_payload = TokenPayload(
+        sub=str(user.id),
+        exp=datetime.utcnow() + timedelta(seconds=JWT_EXP)
+    )
+
+    token = jwt.encode(
+        token_payload.model_dump(),
+        JWT_SECRET,
+        algorithm=JWT_ALGORITHM
+    )
+
+    response = sanic_json(LoginResponse(
+        message="Login successful",
+        user_id=str(user.id)
+    ).model_dump())
+
+    response.add_cookie(
+        key="access_token",
+        value=token,
+        httponly=True,
+        max_age=JWT_EXP,
+        samesite="Strict",
+        path="/",
+    )
+
+    response.add_cookie(
+        key='user_id',
+        value=str(user.id)
+    )
+
+    return response
